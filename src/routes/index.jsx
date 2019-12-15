d537539e0d5c318ffb7f12a4c0c1b06cf3f7dcc3
jianghaoran116 <jianghaoran@outlook.com>
2019-12-15 14:57:05 +0800

    [FIX] 修改路径

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -18,9 +18,7 @@ const NoMatch = lazy(() => import(/* webpackChunkName:'404-chunk' */'view/404/')
 class Index extends Component {
   render() {
     return (
-      <Router
-        basename="/spa"
-      >
+      <Router>
         <Suspense
           fallback={<Loading />}
         >
