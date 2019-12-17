5501d6bda38f30d03b224260e5f8237b6b516dc4
haoran <jianghaoran@outlook.com>
2019-12-17 13:55:44 +0800

    [FEAT] 设置跨域

--- server/content.js
+++ server/content.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const Router = express.Router();
 
-Router.get('/detail', (req, res) => res.json([
-  {
-    title: 'title-1',
-  },
-  {
-    title: 'title-2',
-  },
-]));
+Router.get('/detail', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.curlyhair.cn');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  res.json([
+    {
+      title: 'title-1',
+    },
+    {
+      title: 'title-2',
+    },
+  ]);
+});
 
 module.exports = Router;
