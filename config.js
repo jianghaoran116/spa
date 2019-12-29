66c4773eb13a34ca9901c8899975ffd80f153106
jianghaoran116 <jianghaoran@outlook.com>
2019-12-29 22:46:04 +0800

    public path

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   // react browserRouter basename - FE
   base_name_dev: '',
-  base_name_prod: '/spa', // www.xxxxx.com/spa -> /spa
+  base_name_prod: '/', // www.xxxxx.com/spa -> /spa
 
   // HTML meta data
   page: {
