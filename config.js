bfe4e086c8fa4708a1616967e8ae45fadc99d037
jianghaoran116 <jianghaoran@outlook.com>
2020-03-05 00:04:08 +0800

    修改线上的请求接口

--- config.js
+++ config.js
@@ -39,6 +39,6 @@ module.exports = {
     // server_host_dev: 'http://localhost:9093/',
     'x-auth-token': '86d3456d-e927-4411-99f3-9f8337626533',
     server_host_dev: 'https://api.morningtogether.com/admin-service/',
-    server_host_prod: 'http://123.207.172.63:9093/',
+    server_host_prod: 'https://api.morningtogether.com/admin-service/',
   },
 };
